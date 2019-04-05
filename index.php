b4ae5c89cf9738a9d7407c9b78cb45f78fe305bf
DESKTOP-4KL961H\chus <beronia@yahoo.com>
2019-04-05 18:17:04 +0200

    Modificaciones para incluir formulario de contacto

--- index.php
+++ index.php
@@ -5,14 +5,16 @@
 </head>
 <body>
 
-<h1>This is a Heading</h1>
+<h1>Esta es la página PRINCIPAL</h1>
 <p>This is a paragraph.</p>
 <?php
    echo "hola esta es mi versión 3";
-   echo "LA vida de la rama master va a su bola"
-   echo "hola esta es una modificación 1 de la versión 3"
-   echo "En master siguen haciendose desarrollos por parte de otros"
-   echo "hola esta es la modificación 2 de la versión 3"
+   echo "LA vida de la rama master va a su bola";
+   echo "hola esta es una modificación 1 de la versión 3";
+   echo "En master siguen haciendose desarrollos por parte de otros";
+   echo "hola esta es la modificación 2 de la versión 3":
+
 ?>
+<A href="contacto.html">Enlace a contacto.</A>
 </body>
 </html> 
