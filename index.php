a7b40d3787ef25f97eb88431721b8caeaf018ea6
DESKTOP-4KL961H\chus <beronia@yahoo.com>
2019-04-05 18:03:09 +0200

    Establecimiento de base común

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
 <?php
    echo "hola esta es mi versión 3";
    echo "LA vida de la rama master va a su bola"
@@ -5,3 +14,5 @@
    echo "En master siguen haciendose desarrollos por parte de otros"
    echo "hola esta es la modificación 2 de la versión 3"
 ?>
+</body>
+</html> 
