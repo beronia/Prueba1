5678711d0bbbc6d78da2fdb8f6507ec0284a92b9
DESKTOP-4KL961H\chus <beronia@yahoo.com>
2019-04-05 17:46:17 +0200

    Cambios 2 para versión 3 hechos

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
    echo "hola esta es mi versión 3";
    echo "LA vida de la rama master va a su bola"
-   echo "hola esta es una modificación 1 de la versión 3";
+   echo "hola esta es una modificación 1 de la versión 3"
+   echo "hola esta es la modificación 2 de la versión 3"
 ?>
